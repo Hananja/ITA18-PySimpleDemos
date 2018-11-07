bdbd31885466f01ebfd788068ca9e9f53f81409b
hananja <e.loos@schule.bremen.de>
2018-11-07 14:51:11 +0100

    Anfang: Callstack (Addition und Multiplikation)

--- functions.py
+++ functions.py
@@ -1,15 +1,21 @@
 # Demonstration von Funktionen
 # 01.11.2018, elo
 
+# Addition
 def add(value1:int, value2:int) -> int:
     result = value1 + value2
     return result
 
+# Multiplikation
 def mul(value1:int, value2:int) -> int:
-    pass
+    result = 0
+    while value1 > 0:
+        result = add(result, value2)  # addiert value2 zu result
+        value1 = value1 - 1           # verkleinert um 1
+    return result
 
 a : int = 1
 b : int = 2
-c : int = add(a, b)
-print(c)
 print(add(a, b))
+c : int = 3
+print(mul(c, 5))
